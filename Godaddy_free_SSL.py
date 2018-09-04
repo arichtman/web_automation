75dfcb5e06c082669c60ed5828d10f06abc729df
Ariel Richtman <ariel@richtman.com.au>
2018-09-04 18:53:29 +0930

    updated for python 3.6+

--- Godaddy_free_SSL.py
+++ Godaddy_free_SSL.py
@@ -4,22 +4,21 @@ from selenium.webdriver.common.by import By
 import time
 import glob
 import os
-from win32api import GetSystemMetrics
 
-download_directory = ""
+download_directory = "" #POPULATE
 
 def get_latest_downloads(filter):
   files_path = os.path.join(download_directory, filter)
   files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
   return files
- 
-urls = ["mail.richtman.com.au", "richtman.com.au", "www.richtman.com.au"]
+
+urls = ["foo.com", "bar.com"] #POPULATE
 domains = "+".join(urls)
   
 free_ssl = webdriver.Chrome()
 free_ssl.set_window_position(0,0)
-free_ssl.set_window_size(GetSystemMetrics(0)/2,GetSystemMetrics(1)*.8)
 free_ssl.get("https://www.sslforfree.com/create?domains=" + domains)
+time.sleep(10)
 #select manual ftp
 elems = free_ssl.find_elements(By.XPATH,'//*[@id="content_create_validation_methods"]/div[1]/a[2]')
 elems[0].click()
@@ -36,27 +35,27 @@ for x in range(1,len(urls)+1):
 #get paths of downloaded files
 files = get_latest_downloads('*')
 
-
 godaddy = webdriver.Chrome()
-godaddy.set_window_position(GetSystemMetrics(0)/2,0)
-godaddy.set_window_size(GetSystemMetrics(0)/2,GetSystemMetrics(1)*.8)
-godaddy.get("http://richtman.com.au/cpanel")
+godaddy.get("http://yoursite.com/cpanel") #POPULATE
 time.sleep(5)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="user"]')
-elems[0].send_keys("")
+elems[0].send_keys("") #POPULATE
 elems = godaddy.find_elements(By.XPATH,'//*[@id="pass"]')
-elems[0].send_keys("")
+elems[0].send_keys("") #POPULATE
 elems = godaddy.find_elements(By.XPATH,'//*[@id="login_submit"]')
 elems[0].click()
+time.sleep(20)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="item_file_manager"]')
 elems[0].click()
+time.sleep(2)
+#may remember last value - check if unchecked?
 elems = godaddy.find_elements(By.XPATH,'//*[@id="optionselect_showhidden"]')
 elems[0].click()
 elems = godaddy.find_elements(By.XPATH,'//*[@id="dirselect_webroot"]')
 elems[0].click()
 elems = godaddy.find_elements(By.XPATH,'//*[@id="optionselect_go"]')
 elems[0].click()
-
+time.sleep(5)
 #move to file manager window
 godaddy.switch_to_window(godaddy.window_handles[1])
 
@@ -64,6 +63,7 @@ elems = godaddy.find_elements(By.XPATH,'//*[@id="location"]')
 elems[0].send_keys("/.well-known/acme-challenge")
 elems = godaddy.find_elements(By.XPATH,'//*[@id="btnGo"]')
 elems[0].click()
+time.sleep(2)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="action-upload"]/a')
 elems[0].click()
 
@@ -133,10 +133,29 @@ elems = godaddy.find_elements(By.XPATH,'//*[@id="fetch-domain"]')
 elems[0].click()
 time.sleep(15)
 
+files = get_latest_downloads('certificate.crt')
+with open(files[0], 'r') as myfile:
+    data=myfile.read()
+
+
+elems = godaddy.find_elements(By.XPATH,'//*[@id="sslcrt"]')
+elems[0].clear()
+time.sleep(2)
+elems[0].send_keys(data)
+time.sleep(15)
+
+elems = godaddy.find_elements(By.XPATH,'//*[@id="fetch-cert"]')
+elems[0].click() #click populate from cert
+time.sleep(5)
+
 files = get_latest_downloads('private.key')
 with open(files[0], 'r') as myfile:
     data=myfile.read()
+
+
 elems = godaddy.find_elements(By.XPATH,'//*[@id="sslkey"]')
+elems[0].clear()
+time.sleep(2)
 elems[0].send_keys(data)
 time.sleep(15)
 
@@ -148,4 +167,3 @@ elems[0].click()
 
 godaddy.close()
 free_ssl.close()
-
