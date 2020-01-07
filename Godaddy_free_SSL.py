3f3bde25056e61ac0afbf24830f71ebc5398de00
Ariel Richtman <ariel@richtman.com.au>
2020-01-07 20:54:24 +1030

    updated back to working

--- Godaddy_free_SSL.py
+++ Godaddy_free_SSL.py
@@ -4,27 +4,36 @@ from selenium.webdriver.common.by import By
 import time
 import glob
 import os
+import autoit
+import zipfile
 
-download_directory = "" #POPULATE
+download_directory = r"" 
+urls = ["foo.com", "bar.com"]
+chromedriver_path = r""
+cpanel_url = r""
+cpanel_username = r""
+cpanel_password = r""
 
 def get_latest_downloads(filter):
   files_path = os.path.join(download_directory, filter)
   files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
   return files
 
-urls = ["foo.com", "bar.com"] #POPULATE
 domains = "+".join(urls)
-  
-free_ssl = webdriver.Chrome()
+
+free_ssl = webdriver.Chrome(chromedriver_path)
 free_ssl.set_window_position(0,0)
 free_ssl.get("https://www.sslforfree.com/create?domains=" + domains)
 time.sleep(10)
+
 #select manual ftp
 elems = free_ssl.find_elements(By.XPATH,'//*[@id="content_create_validation_methods"]/div[1]/a[2]')
 elems[0].click()
+
 #click manually verify
 elems = free_ssl.find_elements(By.XPATH,'//*[@id="content_create_validation_methods"]/div[2]/div[2]/form/button')
 elems[0].click()
+time.sleep(5)
 
 #download files
 for x in range(1,len(urls)+1):
@@ -35,44 +44,75 @@ for x in range(1,len(urls)+1):
 #get paths of downloaded files
 files = get_latest_downloads('*')
 
-godaddy = webdriver.Chrome()
-godaddy.get("http://yoursite.com/cpanel") #POPULATE
+godaddy = webdriver.Chrome(chromedriver_path)
+godaddy.get(cpanel_url) #POPULATE
 time.sleep(5)
+
+#account for chrome cert warning
+elems = godaddy.find_elements(By.XPATH,'//*[@id="details-button"]')
+elems[0].click()
+elems = godaddy.find_elements(By.XPATH,'//*[@id="proceed-link"]')
+elems[0].click()
+time.sleep(5)
+
 elems = godaddy.find_elements(By.XPATH,'//*[@id="user"]')
-elems[0].send_keys("") #POPULATE
+elems[0].send_keys(cpanel_username)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="pass"]')
-elems[0].send_keys("") #POPULATE
+elems[0].send_keys(cpanel_password)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="login_submit"]')
 elems[0].click()
 time.sleep(20)
 elems = godaddy.find_elements(By.XPATH,'//*[@id="item_file_manager"]')
 elems[0].click()
-time.sleep(2)
+time.sleep(5)
+
+#open settings
+elems = godaddy.find_elements(By.XPATH,'//*[@id="btnSettings"]')
+elems[0].click()
+
 #may remember last value - check if unchecked?
 elems = godaddy.find_elements(By.XPATH,'//*[@id="optionselect_showhidden"]')
 elems[0].click()
 elems = godaddy.find_elements(By.XPATH,'//*[@id="dirselect_webroot"]')
 elems[0].click()
+
+#save settings
+
 elems = godaddy.find_elements(By.XPATH,'//*[@id="optionselect_go"]')
 elems[0].click()
 time.sleep(5)
+
 #move to file manager window
 godaddy.switch_to_window(godaddy.window_handles[1])
 
+#switch to public_html/.well-known/acme-challenge
 elems = godaddy.find_elements(By.XPATH,'//*[@id="location"]')
 elems[0].send_keys("/.well-known/acme-challenge")
 elems = godaddy.find_elements(By.XPATH,'//*[@id="btnGo"]')
 elems[0].click()
 time.sleep(2)
+
+#delete anything existing
+elems = godaddy.find_elements(By.XPATH,'//*[@id="action-selectall"]')
+elems[0].click()
+elems = godaddy.find_elements(By.XPATH,'//*[@id="action-delete"]')
+elems[0].click()
+elems = godaddy.find_elements(By.XPATH,'//*[@id="trash"]/div[3]/span/button[1]')
+elems[0].click()
+time.sleep(2)
+
 elems = godaddy.find_elements(By.XPATH,'//*[@id="action-upload"]/a')
 elems[0].click()
 
 #upload files
 godaddy.switch_to_window(godaddy.window_handles[2])
-import autoit
+time.sleep(2)
+
+elems = godaddy.find_elements(By.XPATH,'//*[@id="overwrite_checkbox"]')
+elems[0].click()
 
 for x in range(0,len(urls)):
-  elems = godaddy.find_elements(By.XPATH,'//*[@id="file%d"]' % x)
+  elems = godaddy.find_elements(By.XPATH,'//*[@id="uploader_button"]')
   elems[0].click()
   time.sleep(2)
   autoit.win_active("Open")
@@ -80,8 +120,7 @@ for x in range(0,len(urls)):
   autoit.control_send("Open","Edit1","{ENTER}")
   time.sleep(5)
 
-
-elems = godaddy.find_elements(By.XPATH,'//*[@id="backTo"]')
+elems = godaddy.find_elements(By.XPATH,'//*[@id="lnkReturn"]')
 elems[0].click()
 godaddy.switch_to_window(godaddy.window_handles[1])
 godaddy.close()
@@ -89,6 +128,7 @@ godaddy.close()
 #back to ssl for free to generate
 elems = free_ssl.find_elements(By.XPATH,'//*[@id="content_create_manual_output"]/form/button')
 elems[0].click()
+
 time.sleep(30)
 #now to download all those
 elems = free_ssl.find_elements(By.XPATH,'//*[@id="certificate_download"]')
@@ -97,7 +137,7 @@ time.sleep(5)
 
 #extract our download
 files = get_latest_downloads('*.zip')
-import zipfile
+
 zip_ref = zipfile.ZipFile(files[0], 'r')
 zip_ref.extractall(download_directory)
 zip_ref.close()
@@ -124,6 +164,7 @@ elems = godaddy.find_elements(By.XPATH,'//*[@id="lnkReturn"]')
 elems[0].click()
 elems = godaddy.find_elements(By.XPATH,'//*[@id="lnkReturn"]')
 elems[0].click()
+
 #manage/install ssl
 elems = godaddy.find_elements(By.XPATH,'//*[@id="lnkInstall"]')
 elems[0].click()
@@ -144,8 +185,9 @@ time.sleep(2)
 elems[0].send_keys(data)
 time.sleep(15)
 
+#click populate from cert
 elems = godaddy.find_elements(By.XPATH,'//*[@id="fetch-cert"]')
-elems[0].click() #click populate from cert
+elems[0].click()
 time.sleep(5)
 
 files = get_latest_downloads('private.key')
@@ -166,4 +208,5 @@ elems = godaddy.find_elements(By.XPATH,'//*[starts-with(@id,"yui-gen")]/div[3]/s
 elems[0].click()
 
 godaddy.close()
+free_ssl.close()
 free_ssl.close()
